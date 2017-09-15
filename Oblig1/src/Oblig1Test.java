6fd37e29ee3f1719d549effb9e809171eb19f52d
Yanislav Zagorov <yanislavzagorov@gmail.com>
2017-09-15 12:32:47 +0200

    Added missing curly bracket

--- Oblig1/src/Oblig1Test.java
+++ Oblig1/src/Oblig1Test.java
@@ -15,10 +15,8 @@ Oblig 1 sendes inn!
 
 import java.util.*;
 
-public class Oblig1Test
-{
-	public static void main(String[] args)
-	{
+public class Oblig1Test {
+	public static void main(String[] args) {
 		int antallFeil = 0;
 
 		antallFeil += oppgave1();
@@ -29,12 +27,9 @@ public class Oblig1Test
 		//antallFeil += oppgave6();
 		//antallFeil += oppgave7();
 
-		if (antallFeil == 0)
-		{
+		if (antallFeil == 0) {
 			System.out.println("Gratulerer!! Du passerte testen!");
-		}
-		else
-		{
+		} else {
 			System.out.println("Må forbedres! Du har minst "
 					+ antallFeil + " feil eller svakheter!");
 		}
@@ -42,61 +37,52 @@ public class Oblig1Test
 
 	///// Oppgave 1 //////////////////////////////////////
 
-	public static int oppgave1()
-	{
+	public static int oppgave1() {
 		int antallFeil = 0;
 
 		boolean unntak = false;
 		int[] tom = {};
-		try
-		{
+		try {
 			Oblig1.min(tom);  // kaller maks-metoden
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			unntak = true;
-			if (!(e instanceof java.util.NoSuchElementException))
-			{
+			if (!(e instanceof java.util.NoSuchElementException)) {
 				System.out.println("Opgave 1: a) Feil unntak for en tom tabell!");
 				antallFeil++;
 			}
 		}
 
-		if (!unntak)
-		{
+		if (!unntak) {
 			System.out.println("Opgave 1: b) Kast unntak for en tom tabell!");
 			antallFeil++;
 		}
 
 		int[] a = {3};
-		int[] b = {5,2,8,4,7,6};
-		int[] c = {5,4,3,2,1};
-		int[] d = {1,2,3,4,5};
+		int[] b = {5, 2, 8, 4, 7, 6};
+		int[] c = {5, 4, 3, 2, 1};
+		int[] d = {1, 2, 3, 4, 5};
 		if (Oblig1.min(a) != 3 || Oblig1.min(b) != 2 ||
-				Oblig1.min(c) != 1 || Oblig1.maks(d) != 1)
-		{
+				Oblig1.min(c) != 1 || Oblig1.maks(d) != 1) {
 			System.out.println("Oppgave 1: c) Min-metoden: Feil resultat!");
 			antallFeil++;
 		}
 
-		int[] e = {1,4,3,7,6,5,10,2,9,8};
-		int[] f = {1,3,4,6,5,7,2,9,8,10};
+		int[] e = {1, 4, 3, 7, 6, 5, 10, 2, 9, 8};
+		int[] f = {1, 3, 4, 6, 5, 7, 2, 9, 8, 10};
 
 		Oblig1.min(e);
-		if(!Arrays.equals(e,f))
-		{
+		if (!Arrays.equals(e, f)) {
 			System.out.println("Oppgave 1: d) Min-metoden: feil i ombyttingene!");
 			antallFeil++;
 		}
 
-		a = new int[] {6,5,4,3,2,1};
-		b = new int[] {1,2,3,4,5};
-		c = new int[] {4, 9, 3, 6, 1, 5, 7, 8, 10, 2};
-		d = new int[] {2, 5, 8, 4, 3, 10, 1, 7, 6, 9};
+		a = new int[]{6, 5, 4, 3, 2, 1};
+		b = new int[]{1, 2, 3, 4, 5};
+		c = new int[]{4, 9, 3, 6, 1, 5, 7, 8, 10, 2};
+		d = new int[]{2, 5, 8, 4, 3, 10, 1, 7, 6, 9};
 
 		if (Oblig1.ombyttinger(a) != 5 || Oblig1.ombyttinger(b) != 0
-				|| Oblig1.ombyttinger(c) != 7 || Oblig1.ombyttinger(d) != 6)
-		{
+				|| Oblig1.ombyttinger(c) != 7 || Oblig1.ombyttinger(d) != 6) {
 			System.out.println("Oppgave 1: e) Feil opptelling i ombyttingsmetoden!");
 			antallFeil++;
 		}
@@ -105,20 +91,16 @@ public class Oblig1Test
 
 ///// Oppgave 2 //////////////////////////////////////
 
-	public static int oppgave2()
-	{
+	public static int oppgave2() {
 		int antallFeil = 0;
 
 		int[] a = {};
 		int[] b = {1};
-		int[] c = {1,2,3,4,5,4};
+		int[] c = {1, 2, 3, 4, 5, 4};
 
-		try
-		{
+		try {
 			Oblig1.modus(b);  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println
 					("Oppgave 2: a) Ikke unntak for tabell med 0 eller 1 verdi!");
 			antallFeil++;
@@ -126,39 +108,33 @@ public class Oblig1Test
 
 		boolean unntak = false;
 
-		try
-		{
+		try {
 			Oblig1.modus(a);  // kaller metoden
 			Oblig1.modus(c);  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			unntak = true;
-			if (!(e instanceof IllegalStateException))
-			{
+			if (!(e instanceof IllegalStateException)) {
 				System.out.println
 						("Oppgave 2: b) Feil unntak for en tom eller usortert tabell!");
 				antallFeil++;
 			}
 		}
 
-		if (!unntak)
-		{
+		if (!unntak) {
 			System.out.println
 					("Oppgave 2: c) Kast et unntak for en tom eller usortert tabell!");
 			antallFeil++;
 		}
 
-		int[] d = {1,1};
-		int[] e = {1,2,3,4,5,6,7,7};
-		int[] f = {1,1,2,2,2,3,4,4,5,6,6,6,6,7};
+		int[] d = {1, 1};
+		int[] e = {1, 2, 3, 4, 5, 6, 7, 7};
+		int[] f = {1, 1, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6, 7};
 
 		if (Oblig1.modus(a) != 0
 				|| Oblig1.modus(b) != 1
 				|| Oblig1.modus(d) != 1
 				|| Oblig1.modus(e) != 7
-				|| Oblig1.modus(f) != 6)
-		{
+				|| Oblig1.modus(f) != 6) {
 			System.out.println("Oppgave 2: d) Metoden gir feil resultat!");
 			antallFeil++;
 		}
@@ -212,120 +188,98 @@ public class Oblig1Test
 		}
 	}
 
-		///// Oppgave 4 //////////////////////////////////////
+	///// Oppgave 4 //////////////////////////////////////
 
-	public static int oppgave4()
-	{
+	public static int oppgave4() {
 		int antallFeil = 0;
 
 		int[] a = {};   // skal ikke kaste unntak her!
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: a) Ikke unntak for en tom tabell!");
 			antallFeil++;
 		}
 
-		a = new int[] {5};
+		a = new int[]{5};
 		int[] b = {5};
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: b) Skal ikke kastes unntak her!");
 			antallFeil++;
 		}
 
-		if (!Arrays.equals(a,b))
-		{
+		if (!Arrays.equals(a, b)) {
 			System.out.println
 					("Oppgave 4: c) Metoden gjør feil for en tabell en verdi!");
 			antallFeil++;
 		}
 
-		a = new int[] {4};
-		b = new int[] {4};
+		a = new int[]{4};
+		b = new int[]{4};
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: d) Skal ikke kastes unntak her!");
 			antallFeil++;
 		}
 
-		if (!Arrays.equals(a,b))
-		{
+		if (!Arrays.equals(a, b)) {
 			System.out.println
 					("Oppgave 4: e) Metoden gjør feil for en tabell en verdi!");
 			antallFeil++;
 		}
 
-		a = new int[] {4,2,6,10,8};
-		b = new int[] {2,4,6,8,10};
+		a = new int[]{4, 2, 6, 10, 8};
+		b = new int[]{2, 4, 6, 8, 10};
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: f) Det går galt hvis det kun er partall!");
 			antallFeil++;
 		}
 
-		if (!Arrays.equals(a,b))
-		{
+		if (!Arrays.equals(a, b)) {
 			System.out.println
 					("Oppgave 4: g) Det blir feil hvis det kun er partall!");
 			antallFeil++;
 		}
 
-		a = new int[] {9,5,3,1,7};
-		b = new int[] {1,3,5,7,9};
+		a = new int[]{9, 5, 3, 1, 7};
+		b = new int[]{1, 3, 5, 7, 9};
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: h) Det går galt hvis det kun er oddetall!");
 			antallFeil++;
 		}
 
-		if (!Arrays.equals(a,b))
-		{
+		if (!Arrays.equals(a, b)) {
 			System.out.println
 					("Oppgave 4: i) Det blir feil hvis det kun er oddetall!");
 			antallFeil++;
 		}
 
-		a = new int[] {1,2,3,4,5,6};
-		b = new int[] {1,3,5,2,4,6};
+		a = new int[]{1, 2, 3, 4, 5, 6};
+		b = new int[]{1, 3, 5, 2, 4, 6};
 		boolean flere = true;
 
-		do
-		{
+		do {
 			int[] c = a.clone();
 			Oblig1.delsortering(c);
 
-			if (!Arrays.equals(c,b))
-			{
+			if (!Arrays.equals(c, b)) {
 				System.out.println
 						("Oppgave 4: j) Gitt tabell:     " + Arrays.toString(a));
 				System.out.println
@@ -338,38 +292,31 @@ public class Oblig1Test
 			}
 		} while (nestePermutasjon(a));
 
-		a = new int[] {-4,-1,3,0,2,-3,-2,4,1};
-		b = new int[] {-3,-1,1,3,-4,-2,0,2,4};
+		a = new int[]{-4, -1, 3, 0, 2, -3, -2, 4, 1};
+		b = new int[]{-3, -1, 1, 3, -4, -2, 0, 2, 4};
 
-		try
-		{
+		try {
 			Oblig1.delsortering(a);  // kaller metoden
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			System.out.println
 					("Oppgave 4: k) Skal ikke kastes unntak her!");
 			antallFeil++;
 		}
 
-		if (!Arrays.equals(a,b))
-		{
+		if (!Arrays.equals(a, b)) {
 			System.out.println
 					("Oppgave 4: l) Metoden gjør feil for negative verdier!");
 			antallFeil++;
 		}
 
-		if (antallFeil == 0)
-		{
+		if (antallFeil == 0) {
 			a = randPerm(100000);
 			long tid = System.currentTimeMillis();
 			Oblig1.delsortering(a);
 			tid = System.currentTimeMillis() - tid;
 
-			for (int i = 0; i < 50000; i++)
-			{
-				if (a[i] != 2*i + 1)
-				{
+			for (int i = 0; i < 50000; i++) {
+				if (a[i] != 2 * i + 1) {
 					System.out.println
 							("Oppgave 4: m) Feil resultat for denne tabellen!");
 					antallFeil++;
@@ -377,10 +324,8 @@ public class Oblig1Test
 				}
 			}
 
-			for (int i = 50000; i < 100000; i++)
-			{
-				if (a[i] != 2*(i - 49999))
-				{
+			for (int i = 50000; i < 100000; i++) {
+				if (a[i] != 2 * (i - 49999)) {
 					System.out.println
 							("Oppgave 4: n) Feil resultat for denne tabellen!");
 					antallFeil++;
@@ -388,8 +333,7 @@ public class Oblig1Test
 				}
 			}
 
-			if (tid > 100)
-			{
+			if (tid > 100) {
 				System.out.println
 						("Oppgave 4: o) Tid: " + tid + ". Metoden er for ineffektiv!");
 				System.out.println
@@ -403,18 +347,14 @@ public class Oblig1Test
 
 	///// Oppgave 5 //////////////////////////////////////
 
-	public static int oppgave5()
-	{
+	public static int oppgave5() {
 		int antallFeil = 0;
 
 		char[] a = {};
 
-		try
-		{
+		try {
 			Oblig1.rotasjon(a);  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println
 					("Oppgave 5: a) Skal ikke kaste unntak for en tom tabell!!");
 			antallFeil++;
@@ -423,27 +363,24 @@ public class Oblig1Test
 		char[] b = {'A'};
 		char[] b0 = {'A'};
 		Oblig1.rotasjon(b);
-		if (!Arrays.equals(b, b0))
-		{
+		if (!Arrays.equals(b, b0)) {
 			System.out.println("Oppgave 5: b) Feil hvis tabellen har ett element!");
 			antallFeil++;
 		}
 
-		char[] c = {'A','B'};
-		char[] c0 = {'B','A'};
+		char[] c = {'A', 'B'};
+		char[] c0 = {'B', 'A'};
 		Oblig1.rotasjon(c);
-		if (!Arrays.equals(c, c0))
-		{
+		if (!Arrays.equals(c, c0)) {
 			System.out.println("Oppgave 5: c) Feil hvis tabellen har to elementer!");
 			antallFeil++;
 		}
 
-		char[] d = {'A','B','C','D','E','F','G','H','I','J'};
-		char[] d0 = {'J','A','B','C','D','E','F','G','H','I'};
+		char[] d = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+		char[] d0 = {'J', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
 
 		Oblig1.rotasjon(d);
-		if (!Arrays.equals(d, d0))
-		{
+		if (!Arrays.equals(d, d0)) {
 			System.out.println("Oppgave 5: d) Feil hvis tabellen har flere elementer!");
 			antallFeil++;
 		}
@@ -453,18 +390,14 @@ public class Oblig1Test
 
 	///// Oppgave 6 //////////////////////////////////////
 
-	public static int oppgave6()
-	{
+	public static int oppgave6() {
 		int antallFeil = 0;
 
 		char[] a = {};
 
-		try
-		{
-			Oblig1.rotasjon(a,1);  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		try {
+			Oblig1.rotasjon(a, 1);  // kaller metoden
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 6: a) Skal ikke kaste unntak for en tom tabell!!");
@@ -473,104 +406,93 @@ public class Oblig1Test
 
 		char[] b = {'A'};
 		char[] b0 = {'A'};
-		Oblig1.rotasjon(b,0);
-		if (!Arrays.equals(b, b0))
-		{
+		Oblig1.rotasjon(b, 0);
+		if (!Arrays.equals(b, b0)) {
 			System.out.println("Oppgave 6: b) Feil hvis tabellen har ett element!");
 			antallFeil++;
 		}
 
-		Oblig1.rotasjon(b,1);
-		if (!Arrays.equals(b, b0))
-		{
+		Oblig1.rotasjon(b, 1);
+		if (!Arrays.equals(b, b0)) {
 			System.out.println("Oppgave 6: c) Feil hvis tabellen har ett element!");
 			antallFeil++;
 		}
 
-		Oblig1.rotasjon(b,-1);
-		if (!Arrays.equals(b, b0))
-		{
+		Oblig1.rotasjon(b, -1);
+		if (!Arrays.equals(b, b0)) {
 			System.out.println("Oppgave 6: d) Feil hvis tabellen har ett element!");
 			antallFeil++;
 		}
 
-		char[] c = {'A','B'};
-		char[] c0 = {'B','A'};
-		Oblig1.rotasjon(c,1);
+		char[] c = {'A', 'B'};
+		char[] c0 = {'B', 'A'};
+		Oblig1.rotasjon(c, 1);
 
-		if (!Arrays.equals(c, c0))
-		{
+		if (!Arrays.equals(c, c0)) {
 			System.out.println("Oppgave 6: e) Feil hvis tabellen har to elementer!");
 			antallFeil++;
 		}
 
-		c = new char[] {'A','B'};
+		c = new char[]{'A', 'B'};
 
-		Oblig1.rotasjon(c,-1);
-		if (!Arrays.equals(c, c0))
-		{
+		Oblig1.rotasjon(c, -1);
+		if (!Arrays.equals(c, c0)) {
 			System.out.println("Oppgave 6: f) Feil hvis tabellen har to elementer!");
 			antallFeil++;
 		}
 
-		char[] d = {'A','B','C','D','E','F','G','H','I','J'};
-		char[] d0 = {'G','H','I','J','A','B','C','D','E','F'};
+		char[] d = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+		char[] d0 = {'G', 'H', 'I', 'J', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-		Oblig1.rotasjon(d,4);
-		if (!Arrays.equals(d, d0))
-		{
+		Oblig1.rotasjon(d, 4);
+		if (!Arrays.equals(d, d0)) {
 			System.out.println("Oppgave 6: g) Feil hvis tabellen har flere elementer!");
 			antallFeil++;
 		}
 
-		d = new char[]{'A','B','C','D','E','F','G','H','I','J'};
-		Oblig1.rotasjon(d,-6);
-		if (!Arrays.equals(d, d0))
-		{
+		d = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+		Oblig1.rotasjon(d, -6);
+		if (!Arrays.equals(d, d0)) {
 			System.out.println("Oppgave 6: h) Feil hvis tabellen har flere elementer!");
 			antallFeil++;
 		}
 
 		char[] x = new char[100_000];
 		long tid = System.currentTimeMillis();
-		Oblig1.rotasjon(x,99_999);
+		Oblig1.rotasjon(x, 99_999);
 		tid = System.currentTimeMillis() - tid;
 
-		if (tid > 20)
-		{
+		if (tid > 20) {
 			System.out.println("Oppgave 6: i) Metoden "
 					+ "er for ineffektiv. Må forbedres!");
 			antallFeil++;
 		}
 
 		tid = System.currentTimeMillis();
-		Oblig1.rotasjon(x,199_999);
+		Oblig1.rotasjon(x, 199_999);
 		tid = System.currentTimeMillis() - tid;
 
-		if (tid > 20)
-		{
+		if (tid > 20) {
 			System.out.println("Oppgave 6: j) Metoden "
 					+ "er for ineffektiv. Må forbedres!");
 			antallFeil++;
 		}
 
 		tid = System.currentTimeMillis();
-		Oblig1.rotasjon(x,50_000);
+		Oblig1.rotasjon(x, 50_000);
 		tid = System.currentTimeMillis() - tid;
 
-		if (tid > 20)
-		{
+		if (tid > 20) {
 			System.out.println("Oppgave 6: k) Metoden "
 					+ "er for ineffektiv. Må forbedres!");
 			antallFeil++;
 		}
 
 		tid = System.currentTimeMillis();
-		Oblig1.rotasjon(x,-40_000);
+		Oblig1.rotasjon(x, -40_000);
 		tid = System.currentTimeMillis() - tid;
 
-		if (tid > 20)
-		{
+		if (tid > 20) {
 			System.out.println("Oppgave 6: l) Metoden "
 					+ "er for ineffektiv. Må forbedres!");
 			antallFeil++;
@@ -581,63 +503,50 @@ public class Oblig1Test
 
 	///// Oppgave 7 //////////////////////////////////////
 
-	public static int oppgave7()
-	{
+	public static int oppgave7() {
 		int antallFeil = 0;
 		String s = null;
 
-		try
-		{
-			s = Oblig1.flett("","");  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		try {
+			s = Oblig1.flett("", "");  // kaller metoden
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 7a: a) Skal ikke kaste unntak for to tomme tegnstrenger!!");
 			antallFeil++;
 		}
 
-		if (s.compareTo("") != 0)
-		{
+		if (s.compareTo("") != 0) {
 			System.out.println
 					("Oppgave 7a: b) Svaret skal bli lik en tom streng!");
 			antallFeil++;
 		}
 
-		try
-		{
-			s = Oblig1.flett("","AB");  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		try {
+			s = Oblig1.flett("", "AB");  // kaller metoden
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 7a: c) Skal ikke kaste unntak for en tom tegnstreng!!");
 			antallFeil++;
 		}
 
-		if (s.compareTo("AB") != 0)
-		{
+		if (s.compareTo("AB") != 0) {
 			System.out.println
 					("Oppgave 7a: d) Svaret skal bli lik AB");
 			antallFeil++;
 		}
 
-		try
-		{
-			s = Oblig1.flett("AB","");  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		try {
+			s = Oblig1.flett("AB", "");  // kaller metoden
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 7a: e) Skal ikke kaste unntak for en tom tegnstreng!!");
 			antallFeil++;
 		}
 
-		if (s.compareTo("AB") != 0)
-		{
+		if (s.compareTo("AB") != 0) {
 			System.out.println
 					("Oppgave 7a: f) Svaret skal bli lik AB");
 			antallFeil++;
@@ -645,8 +554,7 @@ public class Oblig1Test
 
 		s = Oblig1.flett("A", "BCDEF");
 
-		if (s.compareTo("ABCDEF") != 0)
-		{
+		if (s.compareTo("ABCDEF") != 0) {
 			System.out.println
 					("Oppgave 7a: g) Svaret skal bli lik ABCDEF");
 			antallFeil++;
@@ -654,8 +562,7 @@ public class Oblig1Test
 
 		s = Oblig1.flett("ABCDE", "F");
 
-		if (s.compareTo("AFBCDE") != 0)
-		{
+		if (s.compareTo("AFBCDE") != 0) {
 			System.out.println
 					("Oppgave 7a: h) Svaret skal bli lik AFBCDE");
 			antallFeil++;
@@ -663,8 +570,7 @@ public class Oblig1Test
 
 		s = Oblig1.flett("ACEGIK", "BDFHJLMN");
 
-		if (s.compareTo("ABCDEFGHIJKLMN") != 0)
-		{
+		if (s.compareTo("ABCDEFGHIJKLMN") != 0) {
 			System.out.println
 					("Oppgave 7a: i) Svaret skal bli lik ABCDEFGHIJKLMN");
 			antallFeil++;
@@ -672,57 +578,47 @@ public class Oblig1Test
 
 		String[] a = {};
 
-		try
-		{
+		try {
 			s = Oblig1.flett(a);  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 7b: a) Skal ikke kaste unntak for en tom tabell!");
 			antallFeil++;
 		}
 
-		if (s.compareTo("") != 0)
-		{
+		if (s.compareTo("") != 0) {
 			System.out.println
 					("Oppgave 7b: b) Svaret skal bli lik en tom streng!");
 			antallFeil++;
 		}
 
-		try
-		{
-			s = Oblig1.flett("","ABC","");  // kaller metoden
-		}
-		catch (Exception e)
-		{
+		try {
+			s = Oblig1.flett("", "ABC", "");  // kaller metoden
+		} catch (Exception e) {
 			System.out.println(e);
 			System.out.println
 					("Oppgave 7b: c) Skal ikke kaste unntak for en tom streng!");
 			antallFeil++;
 		}
 
-		if (s.compareTo("ABC") != 0)
-		{
+		if (s.compareTo("ABC") != 0) {
 			System.out.println
 					("Oppgave 7b: d) Svaret skal bli lik ABC");
 			antallFeil++;
 		}
 
-		s = Oblig1.flett("AM ","L","GEDS","ORATKRR","","R TRTE","IO","TGAUU");
+		s = Oblig1.flett("AM ", "L", "GEDS", "ORATKRR", "", "R TRTE", "IO", "TGAUU");
 
-		if (s.compareTo("ALGORITMER OG DATASTRUKTURER") != 0)
-		{
+		if (s.compareTo("ALGORITMER OG DATASTRUKTURER") != 0) {
 			System.out.println
 					("Oppgave 7b: e) Svaret skal bli ALGORITMER OG DATASTRUKTURER!");
 			antallFeil++;
 		}
 
-		s = Oblig1.flett("AFK","BGLP","CHMQT","DINRUW","EJOSVXY");
+		s = Oblig1.flett("AFK", "BGLP", "CHMQT", "DINRUW", "EJOSVXY");
 
-		if (s.compareTo("ABCDEFGHIJKLMNOPQRSTUVWXY") != 0)
-		{
+		if (s.compareTo("ABCDEFGHIJKLMNOPQRSTUVWXY") != 0) {
 			System.out.println
 					("Oppgave 7b: f) Svaret skal bli ABCDEFGHIJKLMNOPQRSTUVWXY!");
 			antallFeil++;
@@ -730,3 +626,4 @@ public class Oblig1Test
 
 		return antallFeil;
 	}
+}
