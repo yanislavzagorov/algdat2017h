49f1257a2a7f6d89a9c913088497c301a30221f9
30bemol <s315316@stud.hioa.no>
2017-09-22 11:48:28 +0200

    finne minst

--- Oblig1/src/Oblig1.java
+++ Oblig1/src/Oblig1.java
@@ -1,8 +1,22 @@
 public class Oblig1 {
 
-	public static int min(int[] tabell){
+	public static void main(String []args){
+		int [] test = {10, 5, 1, 7, 3, 2, 8, 4, 17};
+		System.out.println("minsverdi: " + min(test));
+	}
 
-		return 0;
+	public static int min(int[] a){
+		int minst = 0;
+		for(int i = a.length-1; i > 0; i--){
+			if(a[i] < a[i-1]){
+				minst = a[i];
+				a[i] = a[i-1];
+				a[i-1] = minst;
+			}
+
+		}
+
+		return a[0];
 	}
 
 	public static int ombyttinger(int[] tabell){
