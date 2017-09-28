b44e4add8300747296e2dab9eff222d6da1b1c5f
ismailk97 <kilinc.i@hotmail.no>
2017-09-28 13:11:09 +0200

    Oppg 9 gjort
    oppgave 9

--- Oblig1/src/Oblig1_del2.java
+++ Oblig1/src/Oblig1_del2.java
@@ -5,33 +5,50 @@ public class Oblig1_del2 {
     /* Oppgave 8
 		// Sammenligning av effektivitetene til sorteringsalgoritmer
 	 */
-    public static void tidSorteringer(int[] a){
+    public static void tidSorteringer(int[] a) {
         int[] b = randPerm(4);
         System.out.println(b[0]);
     }
 
-    public static int[] randPerm(int n)
-    {
+    public static int[] randPerm(int n) {
         Random r = new Random();
         int[] a = new int[n];
 
         Arrays.setAll(a, i -> i + 1);
 
-        for (int k = n - 1; k > 0; k--)
-        {
-            int i = r.nextInt(k+1);
-            bytt(a,k,i);
+        for (int k = n - 1; k > 0; k--) {
+            int i = r.nextInt(k + 1);
+            bytt(a, k, i);
         }
 
         return a;
     }
 
-    public static void bytt(int[] a, int i, int j)
-    {
-        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    public static void bytt(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 
     /* Oppgave 9
 		// Summen av to tall er lik x?
 	 */
+    public static int[] sumX(int[] a, int x) {
+        if (Oblig1.sjekkStigende(a) == false) {
+            throw new IllegalArgumentException("ikke sortert");
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                a[i] = i;
+                for (int j = 0; j < a.length; j++) {
+                    if (a[i] + a[j] == x) {
+
+                        return new int[]{a[i], a[j]};
+                    }
+                }
+
+            }
+            return null;
+        }
+    }
 }
+
